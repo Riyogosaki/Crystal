4143b1761865a87ea2e5275be0bedb6a938d729a
Ankush Kumar <ankushkumae467@gmail.com>
2025-10-20 14:59:06 +0530

    good

--- backend/server.js
+++ backend/server.js
@@ -32,8 +32,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/order", orderRoutes);
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 });
 
