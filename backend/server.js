83b331b9ca234f92d2e0abc00f21a94696c09f21
Ankush Kumar <ankushkumae467@gmail.com>
2025-11-28 16:20:18 +0530

    fdgg

--- backend/server.js
+++ backend/server.js
@@ -12,23 +12,20 @@ import orderRoutes from "./routes/order.routes.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000; // fix the order
+const PORT = process.env.PORT || 5000; 
 
 const __dirname = path.resolve();
 
-// Middleware
 app.use(express.json({ limit: "15mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// Cloudinary configuration
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// Routes
 app.use("/api/product", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/auth", authRoutes);
@@ -41,14 +38,11 @@ app.use("/api/order", orderRoutes);
 //     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 //   });
 // }
-
-
-app.use(express.static("frontend/dist"));
-app.use((req,res)=>{
-  res.sendFile(path.join(__dirname,"frontend/dist/index.html"));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
-// Start server
 app.listen(PORT, () => {
   connectDb();
   console.log(`Server is running on port ${PORT}`);
