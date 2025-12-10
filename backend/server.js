1c5dccb64bb7d3cb6263b307aa52830979e9f90b
Ankush Kumar <ankushkumae467@gmail.com>
2025-12-10 21:10:52 +0530

    aaaaaa

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import path from "path";
-// import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 import { connectDb } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
@@ -9,14 +8,17 @@ import cartRoutes from "./routes/cart.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import orderRoutes from "./routes/order.routes.js";
+import cors from "cors";
 
 dotenv.config();
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 5000; 
+app.use(cors({
+  origin: "https://crystal-ashen.vercel.app",
+  credentials: true,
+}));
 
 app.use(express.json({ limit: "15mb" }));
 app.use(express.urlencoded({ extended: true }));
@@ -33,11 +35,6 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/order", orderRoutes);
 
-// Serve frontend in production
-  // app.use(express.static(path.join(__dirname, "../frontend/dist")));
-// app.get(/.*/, (req, res) => {
-  // res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-// });
 
 
 
